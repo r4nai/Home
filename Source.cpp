b63b5b19acf56cdae1647fbf3479f0918bc48c75
Alex O. Vorob'ev <r4nai@outlook.com>
2016-10-25 22:08:35 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define forn(i, n) for(int i = 0; i < (n); ++i)
+#define all(a) (a).begin(),(a).end()
+#define sz(a) (int)(a).size()
 #define push_back pb
 #define make_pair mp
 #define first ff
 #define second ss
-
-#include <bits\stdc++.h>
+/*#include <bits/stdc++.h> //only for C++11 or above (GCC)*/
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <map>
 
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
+typedef long double ld;
 typedef vector<ll> vll;
 typedef pair<ll, ll> pll;
 typedef pair<int, int> pii;
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(false);
-	cout.tie(false);
-#ifdef _DEBUG
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-#endif
-	/*Code*/
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	/*Code here*/
 	return 0;
 }
